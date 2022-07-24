2d72e6af38897a5b058744a87dff74f1482ed539
Hmuriy Monakh <max_08931993@mail.ru>
2022-07-24 23:52:08 +0300

    add task 2 21/1

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -11,16 +11,17 @@ int z2 = Location ("z", "b");
 int Location(string position, string placement)
 {
     Console.Write($"Введите координаты {position} точка {placement}: ");
-    return Convert.ToInt16(Console.ReadLine());
+    return Convert.ToInt32(Console.ReadLine()); //Convert.ToInt16 заменил на 32 можноввести больше значений
 }
 
 double Equation(double x1, double x2, double y1, double y2, double z1, double z2)
 {
 
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
-                  
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));// применил формулу d = Корень из^/ (x1-x2)в квадрате + (y1-y2)в квадрате + (z1-z2)в квадрате
+  //формулу скинули в группе, а Math.Pow в инете увидел: Math.Sqrt - уравнение                
 }
 
 double LineLength =  Math.Round (Equation(x1, x2, y1, y2, z1, z2), 2 );
 
 Console.WriteLine($"Длина отрезка  {LineLength}");
+ 
